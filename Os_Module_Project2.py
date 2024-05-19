75a72a0c34fd01f9602a1f7f59184fbc73ec60f9
Ogün Erdogan <ogun.erdogan23@gmail.com>
2024-05-19 16:04:03 +0200

    19052024

--- Os_Module_Project2.py
+++ Os_Module_Project2.py
@@ -4,18 +4,26 @@ TR- Dosyalari tarihlerine göre ayirma
 '''
 import os
 import time
-#directory = str(input("Please enter a folder directory: "))
+import datetime
+directory = str(input("Please enter a folder directory: "))
 # C:\Users\ogune\Desktop\Software Testing\Python_OS_Module_Project2
-#os.chdir(directory)
-#list_filenames = []
-#list_date = []
-#for i in os.listdir(directory):
-#    if os.path.isdir(os.path.join(directory,i)):
-#        continue
-#    else:
-#        list_filenames.append(i)
-#for files in list_filenames:
-#    if time.ctime
+os.chdir(directory)
+list_filenames = []
+list_date = []
+for i in os.listdir(directory):
+    if os.path.isdir(os.path.join(directory,i)):
+        continue
+    else:
+        list_filenames.append(i)
+for files in list_filenames:
+    c_time = os.path.getmtime(os.path.join(directory,files))
+    print(type(time.ctime(c_time))) # str
+    date_str = time.ctime(c_time)
+    if date_str in list_date:
+        continue
+    else:
+        list_date.append(date_str)
+        print(datetime.date())
 
-c_time = os.path.getmtime(r"C:\Users\ogune\Desktop\Software Testing\Python_OS_Module_Project2\20231001_163105.jpg")
-print(type(time.ctime(c_time)))
+
+
