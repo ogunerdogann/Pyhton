a8e8c943637975e0d7359efb8b438b07a6662b94
Ogün Erdogan <ogun.erdogan23@gmail.com>
2024-06-16 13:52:38 +0200

    16062024

--- Unterricht28_Dunder(Magic)Methods.py
+++ Unterricht28_Dunder(Magic)Methods.py
@@ -30,3 +30,9 @@ print(list1+list2)              # prints [6, 8, 10, 12]
 
 # For more information https://www.geeksforgeeks.org/dunder-magic-methods-python/
      
+'''
+Python Magic methods are the methods starting and ending with double underscores '__'. 
+They are defined by built-in classes in Python and commonly used for operator overloading. 
+
+They are also called Dunder methods, Dunder here means “Double Under (Underscores)”.
+'''
